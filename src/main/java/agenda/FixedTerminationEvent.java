f1950582ee5a8fc4de9062dceddd82cc0b176c72
Laurine2905 <laurine.rat@outlook.fr>
2022-11-17 12:26:19 +0100

    V7 Laurine (travail sur fixedevent isinday (fini à tester)

--- src/main/java/agenda/FixedTerminationEvent.java
+++ src/main/java/agenda/FixedTerminationEvent.java
@@ -11,40 +11,40 @@ import java.time.temporal.ChronoUnit;
  */
 public class FixedTerminationEvent extends RepetitiveEvent {
 
-    private LocalDate end ;
+    private LocalDate end;
     private long nbIteration;
 
     /**
      * Constructs a fixed terminationInclusive event ending at a given date
      *
-     * @param title the title of this event
-     * @param start the start time of this event
-     * @param duration the duration of this event
-     * @param frequency one of :
-     * <UL>
-     * <LI>ChronoUnit.DAYS for daily repetitions</LI>
-     * <LI>ChronoUnit.WEEKS for weekly repetitions</LI>
-     * <LI>ChronoUnit.MONTHS for monthly repetitions</LI>
-     * </UL>
+     * @param title                the title of this event
+     * @param start                the start time of this event
+     * @param duration             the duration of this event
+     * @param frequency            one of :
+     *                             <UL>
+     *                             <LI>ChronoUnit.DAYS for daily repetitions</LI>
+     *                             <LI>ChronoUnit.WEEKS for weekly repetitions</LI>
+     *                             <LI>ChronoUnit.MONTHS for monthly repetitions</LI>
+     *                             </UL>
      * @param terminationInclusive the date when this event ends
      */
     public FixedTerminationEvent(String title, LocalDateTime start, Duration duration, ChronoUnit frequency, LocalDate terminationInclusive) {
-         super(title, start, duration, frequency);
-         end = terminationInclusive;
+        super(title, start, duration, frequency);
+        end = terminationInclusive;
     }
 
     /**
      * Constructs a fixed termination event ending after a number of iterations
      *
-     * @param title the title of this event
-     * @param start the start time of this event
-     * @param duration the duration of this event
-     * @param frequency one of :
-     * <UL>
-     * <LI>ChronoUnit.DAYS for daily repetitions</LI>
-     * <LI>ChronoUnit.WEEKS for weekly repetitions</LI>
-     * <LI>ChronoUnit.MONTHS for monthly repetitions</LI>
-     * </UL>
+     * @param title               the title of this event
+     * @param start               the start time of this event
+     * @param duration            the duration of this event
+     * @param frequency           one of :
+     *                            <UL>
+     *                            <LI>ChronoUnit.DAYS for daily repetitions</LI>
+     *                            <LI>ChronoUnit.WEEKS for weekly repetitions</LI>
+     *                            <LI>ChronoUnit.MONTHS for monthly repetitions</LI>
+     *                            </UL>
      * @param numberOfOccurrences the number of occurrences of this repetitive event
      */
     public FixedTerminationEvent(String title, LocalDateTime start, Duration duration, ChronoUnit frequency, long numberOfOccurrences) {
@@ -53,31 +53,48 @@ public class FixedTerminationEvent extends RepetitiveEvent {
     }
 
     /**
-     *
      * @return the termination date of this repetitive event
      */
 
-    public LocalDate getTerminationDate(){
-        if(end != null){
+    public LocalDate getTerminationDate() {
+        if (end != null) {
             return end;
-        }else {
-            return (this.getStart().plus(getNumberOfOccurrences()-1, getFrequency())).toLocalDate();
+        } else {
+            return (this.getStart().plus(getNumberOfOccurrences() - 1, getFrequency())).toLocalDate();
         }
     }
 
 
     /**
-     *
      * @return the number of occurences of this repetitive event
      */
 
     public long getNumberOfOccurrences() {
-        if(nbIteration == 0){
+        if (nbIteration == 0) {
             return nbIteration;
-        }else{
-           return (long) getDuration().dividedBy((getFrequency());
+        } else {
+            return (long) getDuration().dividedBy((getFrequency());
         }
 
     }
-        
+
+    public boolean isInDay(LocalDate aDay) throws Exception {
+        LocalDateTime start = this.getStart();
+        int occurence = 0;
+        while (aDay.atStartOfDay().isBefore(start)) {
+            if (occurence < this.getNumberOfOccurrences()) {
+                if (this.getStart().equals(aDay.atStartOfDay())) {
+                    return true;
+                } else if (this.getStart().plus(this.getDuration()).isAfter(aDay.atStartOfDay()) && this.getStart().isBefore(aDay.atStartOfDay())) {
+                    return true;
+                } else {
+                    start = start.plus(1, getFrequency());
+                }
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
 }
